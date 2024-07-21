d0aa7714af3491b31b980e2e7943eb3d960b857c
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-07-21 20:19:10 +0700

    feat: add student,course,enrolledCourse schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,13 @@
-import { relations, sql } from "drizzle-orm";
+import { relations} from "drizzle-orm";
 import {
   index,
   integer,
-  pgEnum,
   pgTableCreator,
   timestamp,
   uuid,
   varchar,
-  text,
 } from "drizzle-orm/pg-core";
+
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,85 +17,13 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3-template-app_${name}`);
 
-// KALO EXAMPLE-NYA MAU DIHAPUS, HAPUS AJA YA, CUMAN CONTOH AJA KOK INI SUPAYA BISA MEMANDU KALIAN, SEMANGAT!
-
-// ENUMS EXAMPLE
-export const fakultasEnum = pgEnum("fakultas", [
-  "FITB",
-  "FMIPA",
-  "FSRD",
-  "FTMD",
-  "FTTM",
-  "FTSL",
-  "FTI",
-  "SAPPK",
-  "SBM",
-  "SF",
-  "SITH",
-  "STEI",
-]);
-
-export const roleEnum = pgEnum("role", ["Peserta", "Mentor", "Mamet"]);
-
-// USER TABLE EXAMPLE
-export const users = createTable("user", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  nim: varchar("nim", { length: 255 }).notNull().unique(),
-  role: roleEnum("role").notNull(),
-  fakultas: fakultasEnum("fakultas"),
-  nomorKelompok: integer("nomorKelompok"),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }),
-  password: varchar("password", { length: 255 }).notNull(),
-  phoneNumber: varchar("phoneNumber", { length: 255 }),
-  profilePicture: varchar("profilePicture", { length: 255 }),
-  createdAt: timestamp("createdAt", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-  updatedAt: timestamp("updatedAt", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-});
-
-// USER RELATIONS EXAMPLE
-export const usersRelations = relations(users, ({ many }) => ({
-  assignmentSubmissions: many(assignmentSubmissions),
-}));
-
-// ASSIGNMENT TABLE EXAMPLE
-export const assignments = createTable("assignment", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  title: varchar("title", { length: 255 }).notNull(),
-  description: text("description").notNull(),
-  deadline: timestamp("deadline", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-  files: varchar("files", { length: 255 })
-    .array()
-    .default(sql`ARRAY[]::varchar[]`),
-  createdAt: timestamp("createdAt", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-  updatedAt: timestamp("updatedAt", {
-    mode: "date",
-    withTimezone: true,
-  }).notNull(),
-});
-
-// ASSIGNMENT RELATIONS EXAMPLE
-export const assignmentSubmissions = createTable(
-  "assignmentSubmission",
+// TABLE FOR STUDENTS
+export const students = createTable(
+  "student",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    assignmentId: uuid("assignmentId").notNull(),
-    userNim: varchar("userNim", { length: 255 }).notNull(),
-    files: varchar("files", { length: 255 })
-      .array()
-      .default(sql`ARRAY[]::varchar[]`),
+    id : uuid("id").primaryKey().defaultRandom(),
+    firstName: varchar("fistsName", { length: 255 }).notNull(),
+    lastName: varchar("lastName", { length: 255 }).notNull(),
     createdAt: timestamp("createdAt", {
       mode: "date",
       withTimezone: true,
@@ -105,23 +32,67 @@ export const assignmentSubmissions = createTable(
       mode: "date",
       withTimezone: true,
     }).notNull(),
-  },
-  (submission) => ({
-    userIdIdx: index("submission_userId_idx").on(submission.userNim),
-  }),
-);
 
-// ASSIGNMENT RELATIONS EXAMPLE
-export const assignmentSubmissionsRelations = relations(
-  assignmentSubmissions,
+
+});
+
+// STUDENTS RELATIONS
+export const studentsRelations = relations(students, ({ many }) => ({
+  courses: many(courses),
+}));
+
+// TABLE FOR COURSES 
+export const courses = createTable(
+  'course',
+  {
+    id : uuid("id").primaryKey().defaultRandom(),
+    name : varchar("name", { length: 255 }).notNull(),
+    credits : integer('credits').notNull(),
+    createdAt: timestamp("createdAt", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    updatedAt: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+
+  });
+
+// TABLE FOR COURSES ENROLLMENT 
+
+export const courseEnrollments = createTable(
+  "courseEnrollment",
+  {
+    id : uuid("id").notNull().defaultRandom(),
+    studentId : uuid("studentId").notNull(),
+    courseId : uuid("courseId").notNull(),
+    createdAt: timestamp("createdAt", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    updatedAt: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+    
+  },
+  (enrollment) => ({
+    studentIdIdx: index("submission_studentId_idx").on(enrollment.studentId),
+  }),
+)
+
+// COURSE ENROLLMENT RELATION
+export const courseEnrollmentRelations = relations(
+  courseEnrollments,
   ({ one }) => ({
-    assignment: one(assignments, {
-      fields: [assignmentSubmissions.assignmentId],
-      references: [assignments.id],
+    course: one(courses, {
+      fields: [courseEnrollments.courseId],
+      references: [courses.id],
     }),
-    user: one(users, {
-      fields: [assignmentSubmissions.userNim],
-      references: [users.nim],
+    student: one(students, {
+      fields: [courseEnrollments.studentId],
+      references: [students.id],
     }),
   }),
 );
