4c5be0d1e25723e91e6714c89ebe6f2d3356ed4f
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-13 19:47:06 +0700

    feat:home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 export default async function Home() {
-
-
   return (
-    <div>
-      halo bumi
+    <div className="p-60">
+      <div
+        className="flex animate-spin items-center justify-center text-5xl"
+        style={{ animationDuration: "3s" }}
+      >
+        Aku Kamu Kita 24
+      </div>
     </div>
-
   );
 }
-
